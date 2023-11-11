01126d31b1cff080b87e485b69341227784716a0
Arthur Joppart <arthur@digita.ai>
2023-11-11 19:33:19 +0100

    chore: clear test pkg main.go

--- test/main.go
+++ test/main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"fmt"
-	scraper "scraper/pkg"
-	shared "shared/pkg"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	prods, err := shared.GetLatestProducts(shared.GCSBucket)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Amount of products retrieved: %d\n", len(prods))
-	promos := scraper.GetAllPromotions(prods)
-	fmt.Println("Got promos: ", len(promos))
+	fmt.Println("Hello world")
 }
