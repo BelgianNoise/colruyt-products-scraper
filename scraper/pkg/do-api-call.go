b56867a7bbd20a2238bc665afc856c7fca38b18e
Arthur Joppart <arthur@digita.ai>
2024-03-21 18:29:10 +0100

    add X-CG-APIKey header to requests
    The old endpoint for product information is offline. To use the new endpoint you need to provide a header that is used for bot protection.
    So now this header is first retrieved through a headless browser.

--- scraper/pkg/do-api-call.go
+++ scraper/pkg/do-api-call.go
@@ -6,14 +6,23 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	shared "shared/pkg"
 	"sync"
+	"time"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/stealth"
 )
+
+var userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 OPR/107.0.0.0"
 
 func DoAPICall(
 	page int,
 	size int,
 	useProxy bool,
+	XCGAPIKey string,
 ) (
 	responseObject APIResponse,
 	err error,
@@ -28,7 +37,7 @@ func DoAPICall(
 	queryParams.Set("page", fmt.Sprint(page))
 	queryParams.Set("size", fmt.Sprint(size))
 	queryParams.Set("placeId", ColruytPlaceID)
-	queryParams.Set("sort", "new desc")
+	queryParams.Set("sort", "basicprice asc")
 	requestUrl.RawQuery = queryParams.Encode()
 
 	request, requestErr := http.NewRequest("GET", requestUrl.String(), nil)
@@ -36,7 +45,8 @@ func DoAPICall(
 		return APIResponse{}, requestErr
 	}
 
-	request.Header.Set("Host", ColruytAPIHost)
+	request.Header.Set("User-Agent", userAgent)
+	request.Header.Set("X-Cg-Apikey", XCGAPIKey)
 
 	if page == 1 && size == 1 {
 		fmt.Println("- Doing initial API call")
@@ -51,24 +61,25 @@ func DoAPICall(
 		response, responseErr = http.DefaultClient.Do(request)
 	}
 	if responseErr != nil {
-		return retry(page, size, useProxy)
+		fmt.Println(responseErr.Error())
+		return retry(page, size, useProxy, XCGAPIKey)
 	}
 	defer response.Body.Close()
 
 	// fmt.Printf("[%d] Status code: %d\n", page, response.StatusCode)
 	if response.StatusCode != 200 {
-		return retry(page, size, useProxy)
+		return retry(page, size, useProxy, XCGAPIKey)
 	}
 
 	body, bodyErr := io.ReadAll(response.Body)
 	if bodyErr != nil {
-		return retry(page, size, useProxy)
+		return retry(page, size, useProxy, XCGAPIKey)
 	}
 
 	var apiResponse APIResponse
 	unmarshalErr := json.Unmarshal(body, &apiResponse)
 	if unmarshalErr != nil {
-		return retry(page, size, useProxy)
+		return retry(page, size, useProxy, XCGAPIKey)
 	}
 
 	fmt.Printf("[%d] Call successfull\n", page)
@@ -82,6 +93,7 @@ func retry(
 	page int,
 	size int,
 	useProxy bool,
+	XCGAPIKey string,
 ) (
 	responseObject APIResponse,
 	err error,
@@ -89,14 +101,93 @@ func retry(
 	if mayQuit {
 		return APIResponse{Products: []shared.Product{}}, nil
 	}
-	return DoAPICall(page, size, useProxy)
+	return DoAPICall(page, size, useProxy, XCGAPIKey)
 }
 
 func GetAllProducts() (
 	products []shared.Product,
 	err error,
 ) {
-	return GetAllProductsWithParams(100.0, 20, 250, true)
+	useProxies := false
+	if os.Getenv("USE_PROXY") == "true" {
+		useProxies = true
+	}
+	return GetAllProductsWithParams(100.0, 20, 250, useProxies)
+}
+
+// Retrieve a valid X-CG-APIKey from the xframe.js script.
+// Not providing this header will result in a 401.
+func GetXCGAPIKey() (XCGAPIKey string, err error) {
+	var browser *rod.Browser
+	if os.Getenv("ENV") == "local" {
+		// !!!!!!!! Something is still broken here !!!!!!!!
+		println("====== starting browser")
+
+		// Headless runs the browser on foreground, you can also use flag "-rod=show"
+		// Devtools opens the tab in each new tab opened automatically
+		l := launcher.New().
+			Headless(false).
+			Devtools(true)
+
+		defer l.Cleanup()
+
+		url := l.MustLaunch()
+
+		// Trace shows verbose debug information for each action executed
+		// SlowMotion is a debug related function that waits 2 seconds between
+		// each action, making it easier to inspect what your code is doing.
+		browser := rod.New().
+			ControlURL(url).
+			Trace(true).
+			SlowMotion(2 * time.Second).
+			MustConnect()
+
+		// ServeMonitor plays screenshots of each tab. This feature is extremely
+		// useful when debugging with headless mode.
+		// You can also enable it with flag "-rod=monitor"
+		launcher.Open(browser.ServeMonitor(""))
+
+		println("====== browser started")
+	} else {
+		browser = rod.New().SlowMotion(2 * time.Second).MustConnect()
+	}
+	defer browser.MustClose()
+
+	page := stealth.MustPage(browser)
+	// page := browser.MustPage("")
+	router := page.HijackRequests()
+	apikey := ""
+
+	router.MustAdd("*/search", func(ctx *rod.Hijack) {
+		// Get the value of the X-CG-APIKey header
+		apikey = ctx.Request.Header("X-CG-APIKey")
+		// Continue the request
+		ctx.MustLoadResponse()
+	})
+
+	go router.Run()
+
+	page.MustNavigate("https://colruyt.be/nl")
+	// wait for page to load
+	page.MustWaitLoad()
+	page.MustWaitRequestIdle()
+
+	loopsToGo := 18
+	for {
+		// wait for 3 seconds
+		time.Sleep(5 * time.Second)
+		loopsToGo--
+		if loopsToGo == 0 {
+			break
+		}
+		if apikey != "" {
+			return apikey, nil
+		} else {
+			fmt.Println("Still waiting for API key...")
+		}
+	}
+
+	return "", fmt.Errorf("No API key found after 90 seconds.")
 }
 
 func GetAllProductsWithParams(
@@ -109,11 +200,17 @@ func GetAllProductsWithParams(
 	err error,
 ) {
 
+	APIKey, err := GetXCGAPIKey()
+	if err != nil {
+		return []shared.Product{}, err
+	}
+	fmt.Println("API key retrieved: " + APIKey)
+
 	percentageRequired := percentageRequiredOutOf100 / 100.0
 
 	shared.InitProxyVars()
 
-	initResp, err := DoAPICall(1, 1, useProxy)
+	initResp, err := DoAPICall(1, 1, useProxy, APIKey)
 	if err != nil {
 		return []shared.Product{}, err
 	}
@@ -160,7 +257,7 @@ waitTillWeGotEnoughProducts:
 			go func(page int) {
 				defer wg.Done()
 				defer func() { <-limiter }()
-				responseObject, err := DoAPICall(page, pageSize, useProxy)
+				responseObject, err := DoAPICall(page, pageSize, useProxy, APIKey)
 				if err != nil {
 					fmt.Println(err)
 				}
