3e89900786dd15416ed5c2a37c960953f33a2a2b
Arthur Joppart <arthur@digita.ai>
2024-04-11 16:05:35 +0200

    feat: move promotions scraping to new api endpoint

--- scraper/pkg/global-vars.go
+++ scraper/pkg/global-vars.go
@@ -6,12 +6,10 @@ var ColruytAPIEndpoint = ""
 var ColruytPlaceID = ""
 
 var ColruytPromotionAPIEndpoint = ""
-var ColruytPromotionAPIHost = ""
 
 func InitVariables() {
 	ColruytAPIEndpoint = os.Getenv("COLRUYT_API_ENDPOINT_PRODUCTS")
 	ColruytPromotionAPIEndpoint = os.Getenv("COLRUYT_API_ENDPOINT_PROMOTIONS")
-	ColruytPromotionAPIHost = os.Getenv("COLRUYT_API_ENDPOINT_HOST")
 
 	ColruytPlaceID = os.Getenv("COLRUYT_PLACE_ID")
 
@@ -20,9 +18,6 @@ func InitVariables() {
 	}
 	if ColruytPromotionAPIEndpoint == "" {
 		panic("Missing environment variable COLRUYT_API_ENDPOINT_PROMOTIONS")
-	}
-	if ColruytPromotionAPIHost == "" {
-		panic("Missing environment variable COLRUYT_API_ENDPOINT_HOST")
 	}
 	if ColruytPlaceID == "" {
 		panic("Missing environment variable COLRUYT_PLACE_ID")
